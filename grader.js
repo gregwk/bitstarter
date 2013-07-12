7bb9043de4a9324fec29a6f17f9451510c0f5f2f
Ubuntu <ubuntu@ip-172-31-3-247.us-west-2.compute.internal>
2013-07-12 01:00:51 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -26,6 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "error";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +36,12 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertUrlExists = function(url) {
+    var instr = url.toString();
+    console.log("%s does not exist. Exiting.", instr);
+    process.exit(1);
+} 
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,7 +72,8 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
+        .option('-u, --url <url>', 'URL', clone(assertUrlExists), URL_DEFAULT)
+    .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
